aec4214b0134fcf8d2d6274a1b8ff8aab9f468c1
Mani Movassagh Ghazani <mani.mgh@gmail.com>
2021-07-04 15:18:28 +0200

    1

---
+++ src/main/java/io/mani/RestFullApi/controller/MainController.java
@@ -0,0 +1,45 @@
+package io.mani.RestFullApi.controller;
+
+import io.mani.RestFullApi.modell.Topic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class MainController {
+
+    @Autowired
+    private TopicService topicService;
+
+
+    @RequestMapping("/topics")
+    public List<Topic> TopicController() {
+        return topicService.getAllTopics();
+    }
+
+
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id) {
+        return topicService.getTopic(id);
+    }
+
+
+    @RequestMapping(method = RequestMethod.POST,value = "/topics")
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopics(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic,@PathVariable String id){
+        topicService.updateTopics(id,topic);
+    }
+
+
+
+    @RequestMapping(method = RequestMethod.DELETE,value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id){
+        topicService.deleteTopics(id);
+    }
+}
